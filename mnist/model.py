16c68744b96fde9748135e776713e66b7db258fc
KAYWeiss <weiss.kevin@free.fr>
2019-10-16 16:07:18 +0200

    [model] __init__ and predict methods for the model class.

--- mnist/model.py
+++ mnist/model.py
@@ -1,2 +1,16 @@
+import pickle 
+from sklearn.externals import joblib
+
 class Model:
-    pass
+    def __init__(self):
+        try:
+            self.model = joblib.load("mnist_digits_clf.pkl")
+        except:
+            raise(TypeError)
+
+    def predict(self, image):
+        if image.shape[1] == 784:
+            return self.model.predict(image)
+        else:
+            raise(ValueError)
+    
